842f4607ded3f461e5c33de68b05821765df7369
iTulinov <energiay@gmail.com>
2024-03-07 20:59:34 +0300

    проверил запросик

--- src/run.js
+++ src/run.js
@@ -3,6 +3,25 @@ export default (param={}) => {
     // тут будет запрос к серверу
     console.log("server: ", param)
 
+                $.ajax({
+                    url: "custom_web_template.html?object_id=7151025819625201817",
+                    data: {
+                        'action': "get-objects-for-modal-window",
+                        'catalog': "collaborators",
+                        'fields': "id,fullname,code",
+                        'find': "id,fullname,code",
+                        'value': "Тулинов",
+                        'ids': "",
+                        'user-where': "1=1",
+                        ssql: "",
+                        connection: "",
+                    },
+                    cache: false,
+                    type: 'POST',
+                    dataType: 'JSON',
+                }).done((data) => {
+                    console.log(data);
+                })
     // а это ответ от сервера
     return [{...param, name: "zhora"}]
 }
